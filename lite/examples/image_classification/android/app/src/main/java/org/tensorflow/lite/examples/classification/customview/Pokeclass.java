4c27c9cc6c9a5deb46afd81d37a1c2d82380bb81
deepanshu2301 <deepanshujindal40@gmail.com>
2019-09-11 17:36:51 +0530

    Code

--- lite/examples/image_classification/android/app/src/main/java/org/tensorflow/lite/examples/classification/customview/Pokeclass.java
+++ lite/examples/image_classification/android/app/src/main/java/org/tensorflow/lite/examples/classification/customview/Pokeclass.java
@@ -1,41 +1,74 @@
 package org.tensorflow.lite.examples.classification.customview;
 
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.tensorflow.lite.examples.classification.R;
 
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import static android.app.PendingIntent.getActivity;
+import java.lang.reflect.Type;
 
 public class Pokeclass extends AppCompatActivity {
 
-    String pokemon;
+
+    TextView Name, pokeno,type,ability,attack,height,weight,generation,speed,capture,defense,happy,transform,classif,legend;
+    String pokemonn;
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pokeview);
 
-        pokemon = getIntent().getStringExtra("Pokemon");
-        pokemon = pokemon.substring(0,1).toUpperCase() + pokemon.substring(1);
+        Name = findViewById(R.id.name);
+        pokeno=findViewById(R.id.number);
+        type=findViewById(R.id.type);
+        ability=findViewById(R.id.ability);
+        height=findViewById(R.id.height);
+        attack=findViewById(R.id.attack);
+        weight=findViewById(R.id.weight);
+        generation=findViewById(R.id.generation);
+        speed=findViewById(R.id.speed);
+        capture=findViewById(R.id.capture);
+        defense=findViewById(R.id.defense);
+        happy=findViewById(R.id.happy);
+        transform=findViewById(R.id.transform);
+        classif=findViewById(R.id.classif);
+        legend=findViewById(R.id.legend);
+
+
+        pokemonn = getIntent().getStringExtra("Pokemon");
+        pokemonn = pokemonn.substring(0,1).toUpperCase() + pokemonn.substring(1);
         try {
             JSONObject obj = new JSONObject(readJSONFromAsset());
-//            Toast.makeText(getBaseContext(),""+obj.length(),Toast.LENGTH_SHORT).show();
 
-//                Toast.makeText(getBaseContext(),""+obj.get(pokemon),Toast.LENGTH_SHORT).show();
+            Gson gson = new Gson();
+            Pokemon pok = gson.fromJson(obj.get(pokemonn).toString(), Pokemon.class);
+//            Toast.makeText(getBaseContext(),""+pok.getAbilities(),Toast.LENGTH_SHORT).show();
+
+            Name.setText(pokemonn);
+            pokeno.setText(pok.getPokedex_number());
+            type.setText(pok.getType1());
+            ability.setText(pok.getAbilities());
+            height.setText(pok.getHeight_m());
+            attack.setText(pok.getAttack());
+            weight.setText(pok.getWeight_kg());
+            generation.setText(pok.getGeneration());
+            speed.setText(pok.getSpeed());
+            capture.setText(pok.getCapture_rate());
+            defense.setText(pok.getDefense());
+            happy.setText(pok.getBase_happiness());
+            transform.setText(pok.getBase_egg_steps());
+            classif.setText(pok.getClassfication());
+            legend.setText(pok.getIs_legendary());
 
 
         } catch (JSONException e) {
---
+++ lite/examples/image_classification/android/app/src/main/java/org/tensorflow/lite/examples/classification/customview/Pokemon.java
@@ -0,0 +1,147 @@
+package org.tensorflow.lite.examples.classification.customview;
+
+public class Pokemon {
+    public Pokemon(String abilities, String attack, String base_happiness, String capture_rate, String classfication, String defense, String height_m, String pokedex_number, String speed, String type1, String weight_kg, String generation, String is_legendary, String base_egg_steps) {
+        this.abilities = abilities;
+        this.attack = attack;
+        this.base_happiness = base_happiness;
+        this.capture_rate = capture_rate;
+        this.classfication = classfication;
+        this.defense = defense;
+        this.height_m = height_m;
+        this.pokedex_number = pokedex_number;
+        this.speed = speed;
+        this.type1 = type1;
+        this.weight_kg = weight_kg;
+        this.generation = generation;
+        this.is_legendary = is_legendary;
+        this.base_egg_steps = base_egg_steps;
+    }
+
+    public String getAbilities() {
+        return abilities;
+    }
+
+    public void setAbilities(String abilities) {
+        this.abilities = abilities;
+    }
+
+    public String getAttack() {
+        return attack;
+    }
+
+    public void setAttack(String attack) {
+        this.attack = attack;
+    }
+
+    public String getBase_happiness() {
+        return base_happiness;
+    }
+
+    public void setBase_happiness(String base_happiness) {
+        this.base_happiness = base_happiness;
+    }
+
+    public String getCapture_rate() {
+        return capture_rate;
+    }
+
+    public void setCapture_rate(String capture_rate) {
+        this.capture_rate = capture_rate;
+    }
+
+    public String getClassfication() {
+        return classfication;
+    }
+
+    public void setClassfication(String classfication) {
+        this.classfication = classfication;
+    }
+
+    public String getDefense() {
+        return defense;
+    }
+
+    public void setDefense(String defense) {
+        this.defense = defense;
+    }
+
+    public String getHeight_m() {
+        return height_m;
+    }
+
+    public void setHeight_m(String height_m) {
+        this.height_m = height_m;
+    }
+
+    public String getPokedex_number() {
+        return pokedex_number;
+    }
+
+    public void setPokedex_number(String pokedex_number) {
+        this.pokedex_number = pokedex_number;
+    }
+
+    public String getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(String speed) {
+        this.speed = speed;
+    }
+
+    public String getType1() {
+        return type1;
+    }
+
+    public void setType1(String type1) {
+        this.type1 = type1;
+    }
+
+    public String getWeight_kg() {
+        return weight_kg;
+    }
+
+    public void setWeight_kg(String weight_kg) {
+        this.weight_kg = weight_kg;
+    }
+
+    public String getGeneration() {
+        return generation;
+    }
+
+    public void setGeneration(String generation) {
+        this.generation = generation;
+    }
+
+    public String getIs_legendary() {
+        return is_legendary;
+    }
+
+    public void setIs_legendary(String is_legendary) {
+        this.is_legendary = is_legendary;
+    }
+
+    public String getBase_egg_steps() {
+        return base_egg_steps;
+    }
+
+    public void setBase_egg_steps(String base_egg_steps) {
+        this.base_egg_steps = base_egg_steps;
+    }
+
+    public  String abilities;
+    public  String attack;
+    public String base_happiness;
+    public String capture_rate;
+    public String classfication;
+    public String defense;
+    public String height_m;
+    public String pokedex_number;
+    public String speed;
+    public String type1;
+    public String weight_kg;
+    public String generation;
+    public String is_legendary;
+    public String base_egg_steps;
+}
